ba44e60f0bde625787ff766b9e006a04d0046bb4
Dylan Awalt-Conley <web@dylan.ac>
2021-12-19 23:57:59 -0500

    added functions for sending email directly via AWS SES

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import path from 'path';
 import { exec } from 'child_process';
 import slugify from 'slugify';
 import csv from 'csvtojson';
+import fm from 'front-matter';
+import MarkdownIt from 'markdown-it';
 import nunjucks from 'nunjucks';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import gender from './gender.js';
+import { SES } from '@aws-sdk/client-ses';
+import yaml from 'js-yaml';
 
 const argv = yargs(hideBin(process.argv))
     .options({
@@ -30,11 +34,20 @@ const argv = yargs(hideBin(process.argv))
         format: {
             nargs: 1,
             choices: [ 'txt', 'rtf', 'odt', 'html' ],
-            default: 'txt',
+            implies: 'output'
+        },
+        send: {
+            type: 'boolean'
+        },
+        test: {
+            type: 'string'
         }
     })
     .argv;
 
+const Charset = 'UTF-8';
+const ses = new SES({ region: 'us-east-1' });
+const md = MarkdownIt({ html: true });
 const csvParser = csv({ flatKeys: true });
 
 const [ template, data ] = await Promise.all([
@@ -44,27 +57,62 @@ const [ template, data ] = await Promise.all([
 ]);
 
 const nEnv = nunjucks.configure()
-    .addFilter('gender', gender);
+    .addFilter('gender', gender)
+    .addFilter('yamlList', str =>
+        yaml.dump(str.split(/, */g), { flowLevel: 0 })
+            .replace(/\n$/, ''));
+
+let testEmailQueued = false;
 
 await Promise.all(
     data.map(item => {
-        let msg = nEnv.renderString(template.toString(), item);
-        msg = msg.replace(/(\n\s*){2,}/g, '\n\n');
+        if (argv.test && testEmailQueued)
+            return null;
+        let rendered = nEnv.renderString(template.toString(), item);
+        let { attributes, body, frontmatter } = fm(rendered);
+        body = body.replace(/(\n\s*){2,}/g, '\n\n');
+        let whole = frontmatter + '\n\n' + body;
         let outPath = slugify(Object.values(item)[0]);
-        if (argv.output) {
+        if (argv.send) {
+            let params = {
+                ...attributes,
+                Message: {
+                    Subject: {
+                        Data: attributes.Subject,
+                        Charset,
+                    },
+                    Body: {
+                        Html: {
+                            Data: md.render(body),
+                            Charset,
+                        }
+                    }
+                }
+            };
+            if (argv.test) {
+                delete params.Destination;
+                params.Destination = {
+                    ToAddresses: [ argv.test ]
+                };
+                ses.sendEmail(params);
+                testEmailQueued = true;
+            } else {
+                return ses.sendEmail(params);
+            }
+        } else if (argv.output) {
             outPath = path.join(argv.output, outPath);
             if (argv.format === 'txt') {
-                return fsp.writeFile(`${outPath}.txt`, msg);
+                return fsp.writeFile(`${outPath}.txt`, whole);
             } else {
                 return new Promise((resolve, reject) => {
                     const cmd = `pandoc -f markdown -o ${outPath.replace(/ /g, '\\ ')}.${argv.format}`;
                     const convert = exec(cmd, (e, stdout, stderr) => e ? reject(stderr) : resolve(stdout));
-                    convert.stdin.write(msg);
+                    convert.stdin.write(whole);
                     convert.stdin.end();
                 });
             }
         } else {
-            return process.stdout.write(msg);
+            return process.stdout.write(whole);
         }
     })
 );
